df21029269c5eeebd99d04b9219e289e1cd9e89b
MAHY U DEEN SHAHID <mahyudeenshahidiqbal@gmail.com>
2025-07-20 19:33:40 +0500

    password generator

--- password Generator/src/App.jsx
+++ password Generator/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <div>Password Generator</div>
     </>
   )
 }
